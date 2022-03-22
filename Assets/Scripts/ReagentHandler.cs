a2e344054298657605b9c850a671461e99dfadc4
Superfriend <joseph.m.white@outlook.com>
2022-03-21 19:36:37 -0500

    Updated bottle prefab locations for potion display

--- Assets/Scripts/ReagentHandler.cs
+++ Assets/Scripts/ReagentHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//[ExecuteInEditMode]
 public class ReagentHandler : MonoBehaviour
 {
     [Tooltip("Name of reagent.")]
