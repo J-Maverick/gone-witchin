901f4d77b07991e80b9e962863f2bbd548097e62
Superfriend <joseph.m.white@outlook.com>
2022-03-30 14:51:24 -0500

    Updated camera controller to smooth position and rotation as it moves between points

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,42 +8,102 @@ public class CameraController : MonoBehaviour
     public Transform FishingCameraAnchor;
     public enum CameraLocation {PotionCrafting, Fishing}
     public CameraLocation startingLocation;
+
+    public float cameraMoveTime = 0.3f;
+    public float cameraRotationSlowingFactor = 10f;
+
     private CameraLocation cameraLocation;
+    private Transform target;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private float moveTime = 0f;
+    private float normalizedSlerpTime = 0f;
+
+    private bool isMoving = false;
 
     private void Start()
     {
         if (startingLocation == CameraLocation.PotionCrafting)
         {
-            ToPotionCrafting();
+            ToPotionCrafting(damp:false);
             cameraLocation = CameraLocation.PotionCrafting;
         }
         else if (startingLocation == CameraLocation.Fishing)
         {
-            ToFishing();
+            ToFishing(damp:false);
             cameraLocation = CameraLocation.Fishing;
+        }
+    }
+
+    private void Update()
+    {
+        updateTime();
+        // Smooths the motion as the camera moves from one location to another
+        transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, cameraMoveTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, normalizedSlerpTime);
+
+        checkMoving();
+    }
+
+    private void updateTime()
+    {
+        // Time used for smoothing rotation
+        moveTime += Time.deltaTime;
+        normalizedSlerpTime = moveTime / (cameraMoveTime * cameraRotationSlowingFactor);
+    }
+
+    private void checkMoving()
+    {
+        if (velocity.magnitude > 1)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
         }
     }
 
     public void SwapPositions()
     {
-        if (cameraLocation == CameraLocation.PotionCrafting)
+        if (!isMoving)
         {
-            ToFishing();
-            cameraLocation = CameraLocation.Fishing;
-        }
-        else
-        {
-            ToPotionCrafting();
-            cameraLocation = CameraLocation.PotionCrafting;
+            if (cameraLocation == CameraLocation.PotionCrafting)
+            {
+                ToFishing();
+                cameraLocation = CameraLocation.Fishing;
+            }
+            else
+            {
+                ToPotionCrafting();
+                cameraLocation = CameraLocation.PotionCrafting;
+            }
         }
     }
 
-    void ToPotionCrafting()
+    void ToPotionCrafting(bool damp=true)
     {
-        transform.SetPositionAndRotation(PotionCameraAnchor.position, PotionCameraAnchor.rotation);
+        Debug.Log("Moving Camera to Potion-Making");
+        if (!damp)
+        {
+
+            transform.SetPositionAndRotation(PotionCameraAnchor.position, PotionCameraAnchor.rotation);
+        }
+        target = PotionCameraAnchor;
+        moveTime = 0.0f;
+        normalizedSlerpTime = 0f;
     }
-    void ToFishing()
+
+    void ToFishing(bool damp = true)
     {
-        transform.SetPositionAndRotation(FishingCameraAnchor.position, FishingCameraAnchor.rotation);
+        Debug.Log("Moving Camera to Fishing");
+        if (!damp)
+        { 
+            transform.SetPositionAndRotation(FishingCameraAnchor.position, FishingCameraAnchor.rotation);
+        }
+        target = FishingCameraAnchor;
+        moveTime = 0.0f;
+        normalizedSlerpTime = 0f;
     }
 }
